8bce75eb2ba0797bddd049f00ad1c6bc067f92db
alok-jha0 <99009179+alok-jha0@users.noreply.github.com>
2024-11-25 10:14:18 +0000

    des

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+// basics of javascript 
+const accountId =  23456
+let accountName = "Simon"
+var accountPassword = "lalalala"
+accountCity = "Gurugram"
+let accountState;
+console.log(accountId);
+console.table([accountId,accountName,accountPassword,accountCity,accountState]);
+// var is not used now in js instead we use let
+// to declare 
